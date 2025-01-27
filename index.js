c81f4eb213aa332a87b5e215b398bc31f9e970d4
Sheikh <kenneth.sheikh@hotmail.com>
2025-01-28 00:48:50 +0100

    fixed defined array for message

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname)));
+
+const messages = []; // Define the messages array here
 
 app.post("/api/message", (req, res) => {
   const { name } = req.body;
